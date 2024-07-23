7de2ad29b844b6efb02c3eb73d353a237211904d
Lim Jitt Hing <jhlim.2023@smu.edu.sg>
2024-07-24 00:07:19 +0800

    update format of navigation buttons

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -144,7 +144,7 @@ function MainSection() {
 
 const DottedButton = ({ name }) => {
   return (
-    <button className="w-1/2 rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
+    <button className="w-1/2 justify-self-center rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none">
       {name}
     </button>
   );
